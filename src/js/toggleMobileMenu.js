1588314593a980e2ca7265eee4d054f78523351d
Alex Rhind <mrsrhino2009@gmail.com>
2021-12-09 11:45:41 +0000

    js mobile files  in

---
+++ src/js/toggleMobileMenu.js
@@ -0,0 +1,11 @@
+
+
+/* Toggle show/hide nav menu links onclick the hamburger menu */
+function toggleMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
