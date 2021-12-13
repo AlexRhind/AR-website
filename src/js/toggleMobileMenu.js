a34c05085d6adea921134e49e1323ebce4438acf
Alex Rhind <mrsrhino2009@gmail.com>
2021-12-13 19:50:28 +0000

    Mobile menu in place without JS

--- src/js/toggleMobileMenu.js
+++ src/js/toggleMobileMenu.js
@@ -3,9 +3,9 @@
 /* Toggle show/hide nav menu links onclick the hamburger menu */
 function toggleMenu() {
     var x = document.getElementById("siteLinks");
-    if (x.style.display === "block") {
+  if (x.style.display === "block") {
       x.style.display = "none";
-    } else {
+  } else {
       x.style.display = "block";
     }
   }
